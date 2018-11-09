d4e56e3bbb28d6a3dc56ada76006d4d56cfbb85b
lifei <28223731@qq.com>
2018-11-09 12:36:54 +0800

    add phone model

--- woniudushu/src/pages/detail/detail.vue
+++ woniudushu/src/pages/detail/detail.vue
@@ -1,7 +1,22 @@
 <template>
 <div>
 	<BookInfo :book="book"></BookInfo>
-</div>
+	<div class="comment">
+		<textarea v-model='comment'
+              class='textarea'
+              :maxlength='100'
+              placeholder='请输入图书短评'></textarea>
+		<div class='location'>
+			地理位置：
+			<switch color='#EA5A49' :checked='location' @change='getGeo'></switch>
+			<span class='text-primary'>{{location}}</span>
+		</div>
+		<div class='phone'>
+			手机型号：
+			<switch color='#EA5A49' :checked='phone' @change='getPhone'></switch>
+			<span class='text-primary'>{{phone}}</span>
+		</div>
+	</div>
 </template>
 <script>
 import {
@@ -14,7 +29,9 @@ export default {
 	data() {
 		return {
 			bookid: '',
-			book: {}
+			book: {},
+			phone: '',
+			location: ''
 		}
 	},
 	components: {
@@ -27,6 +44,18 @@ export default {
 			})
 			console.log(bookRsp.data)
 			this.book = bookRsp.data
+		},
+		getGeo() {
+
+		},
+		getPhone(e) {
+			if (e.target.value) {
+				const phoneInfo = wx.getSystemInfoSync()
+				console.log(phoneInfo)
+				this.phone = phoneInfo.model
+			} else {
+				this.phone = ''
+			}
 		}
 	},
 	mounted() {
@@ -36,5 +65,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
+.comment {
+    margin-top: 10px;
+    .textarea {
+        width: 730rpx;
+        height: 200rpx;
+        background: #eee;
+        padding: 10rpx;
+    }
+    .location {
+        margin-top: 10px;
+        padding: 5px 10px;
+    }
+    .phone {
+        margin-top: 10px;
+        padding: 5px 10px;
+    }
+}
 </style>
